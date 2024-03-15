8ed98aaee893ec86434fb46317512387f34050e4
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2024-03-15 23:07:20 +0200

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-extern int _printf(const char *format, ...); // Declare _printf function
-
-int main()
-{
-    _printf("Hello, %s!\n", "world"); // Call _printf function
-    return 0;
-}
